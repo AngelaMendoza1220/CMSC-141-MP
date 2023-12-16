cc92680cdbcf80f10ca5988ad1f4dd300e05c703
Angela Nicole Mendoza <anymendoza1220@gmail.com>
2023-12-16 15:00:06 +0800

    Placed 2 text boxes in the window
    Placed 2 textboxes in the window:
    Left text box is for the code editor
    Right text box is for the console

--- window.py
+++ window.py
@@ -1,29 +1,33 @@
 from tkinter import *
 import basic
 
-# get the code from the text box
+
 def readTheCode():
-    theCode = textbox.get("1.0", "end-1c") # gets the code from the textbox
-    linesOfCode = theCode.split('\n')      # split the content into lines
+    theCode = codeEditor.get("1.0", "end-1c") # gets the code from the codeEditor
+    linesOfCode = theCode.split('\n')         # split the content into lines
     
-    lex = basic.lexer(linesOfCode)         # calls the lexer function to analyze each line
-    for obj in lex:                        # display the type and their values
+    lex = basic.lexer(linesOfCode)            # calls the lexer function to analyze each line
+    printProgramResults(lex)
+
+
+def printProgramResults(results):
+    for obj in results:                       # display the type and their values
         print(obj.type, obj.value, sep=' ')
 
 
-window = Tk() # creates a window
-
-window.title("CMSC141")    # sets the title of the window
-window.geometry("800x800") # sets the size of the window
-
-textbox = Text(window, width = 190, height = 35) # creates and adds textbox to the window
-
-# textbox specifications:
-textbox.configure(background='#1e1e1e', foreground='#007acc', font=('Courier New', 15)) 
-
-textbox.pack() # adds the textbox to the window
+window = Tk()           # creates a window
+window.title("CMSC141") # sets the title of the window
 
 run_button = Button(window, text="Run Code", command=readTheCode) # creates the button
-run_button.pack() # adds the button to the window
+run_button.pack(side=TOP)                                         # adds the button to the window
+
+
+codeEditor = Text(window)                                                                  # creates and adds codeEditor to the window
+codeEditor.configure(background='#1e1e1e', foreground='#a3d5ff', font=('Courier New', 15)) # codeEditor specifications:
+codeEditor.pack(expand=True, fill=BOTH, side=LEFT)                                         # adds the codeEditor to the window
+
+console = Text(window)                                                                                    # creates and adds console to the window
+console.configure(background='#1e1e1e', foreground='#a3d5ff', font=('Courier New', 15), state='disabled') # console specifications:
+console.pack(expand=True, fill=BOTH, side=RIGHT)                                                          # adds the console to the window
 
 window.mainloop() # display window and listens for events
